74c213bc62a64c076708a4b2f0ca1f05014026e5
Diogo Monica <diogo@docker.com>
2015-05-05 19:40:01 -0700

    Added configurable run params

--- docker_kwfs.sh
+++ docker_kwfs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export REMOTE_URL="https://localhost:4443"
-export MOUNTPOINT="/secrets/keywhiz_mount/"
+export MOUNTPOINT="/secrets/kwfs/"
 export KEYWHIZ_USER="keywhiz"
 
 # Create mount
@@ -13,5 +12,4 @@ chmod 640 /dev/fuse
 # This doesn't work with aufs. Need overlayFS to support it.
 setcap 'cap_ipc_lock=+ep' /go/bin/keywhizfs
 
-sudo -u $KEYWHIZ_USER /go/bin/keywhizfs -asuser=$KEYWHIZ_USER -group=$KEYWHIZ_USER -debug=true -ca=/go/src/github.com/square/keywhiz-fs/fixtures/cacert.crt -key=/go/src/github.com/square/keywhiz-fs/fixtures/client.pem $REMOTE_URL $MOUNTPOINT
-
+sudo -u $KEYWHIZ_USER /go/bin/keywhizfs -asuser=$KEYWHIZ_USER -group=$KEYWHIZ_USER $@
